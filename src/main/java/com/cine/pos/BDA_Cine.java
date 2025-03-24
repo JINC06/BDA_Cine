1aa1489224634d23feb69a3ec9e421d5a34f6ac1
Julio Nava <julio.nava@caffenio.com>
2025-03-24 07:41:09 -0700

    [ADD] --> Proyecto base

---
+++ src/main/java/com/cine/pos/BDA_Cine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cine.pos;
+
+/**
+ *
+ * @author julio.nava
+ */
+public class BDA_Cine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
